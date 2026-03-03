5619748583797bb4674a4d282a3ac89270e565ab
eddiebond <eddiebond@outlook.com>
2026-03-03 20:15:32 +0000

    We can have nice og images

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,24 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Midlife Highfive Deepdive",
   description: "9th April 2026 - The Holloway, Norwich",
+  openGraph: {
+    title: "Midlife Highfive Deepdive",
+    description: "9th April 2026 - The Holloway, Norwich",
+    images: [
+      {
+        url: "/opengraph-image.jpg",
+        width: 1200,
+        height: 1200,
+        alt: "Midlife Highfive Deepdive",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Midlife Highfive Deepdive",
+    description: "9th April 2026 - The Holloway, Norwich",
+    images: ["/opengraph-image.jpg"],
+  },
 };
 
 export default function RootLayout({
